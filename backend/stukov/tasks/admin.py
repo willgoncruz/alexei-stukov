5a1b473da6e08ec40e158ca372a9ab0f7254ebb3
Allan Nozomu Fukasawa <allannozomu@gmail.com>
2018-06-02 23:19:59 -0300

    Add tasks to project

--- backend/stukov/tasks/admin.py
+++ backend/stukov/tasks/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
+from .models import Task, Team, Project
+
 # Register your models here.
+
+class TaskModelAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description', 'priority')
+
+    search_fields = ['id', 'name', 'description' ]
+
+class TeamModelAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name',)
+
+    search_fields = ['id', 'name', 'project__name']
+
+class ProjectModelAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description')
+
+    search_fields = ['id', 'name', 'description',]
+
+admin.site.register(Task, TaskModelAdmin)
+admin.site.register(Team, TeamModelAdmin)
+admin.site.register(Project, ProjectModelAdmin)
--- backend/stukov/tasks/models.py
+++ backend/stukov/tasks/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from stukov.users.models import User
 
 # Create your models here.
+
+class Task(models.Model):
+    name = models.CharField(max_length=255, blank=False, null=False)
+    date_limit = models.DateField(blank=False, null=False)
+    description = models.TextField()
+    priority = models.IntegerField()
+
+class Project(models.Model):
+    name = models.CharField(max_length=255, blank=False, null=False)
+    description = models.TextField()
+
+class Team(models.Model):
+    name = models.CharField(max_length=255, blank=False, null=False)
+    project = models.ForeignKey(Project, blank=False, null=False, on_delete=models.CASCADE)
+    users = models.ManyToManyField(User)
+    tasks = models.ManyToManyField(Task)
+
