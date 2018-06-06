51e8283b45955e39b962c6601aaf83725a8382ba
yuriloliveira <yuri12194@gmail.com>
2018-06-06 20:13:18 -0300

    Added responsive Project list

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,10 +1,42 @@
 import React from 'react';
+import request from 'axios';
+import ProjectCard from '../Project/ProjectCard';
+
+const API_URL = 'http://18.228.31.90/api';
 
 class Home extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      projects: []
+    };
+  }
+
+  componentWillMount() {
+    request.get(`${API_URL}/projects/`)
+    .then(response => {
+      this.setState({
+        projects: response.data
+      });
+      console.log(this.state.projects);
+    })
+    .catch(e => {
+
+    });
+  }
+
   render() {
     return (
-      <p id="main-container">
-      </p>
+      <div id="project-container" className="flex-container inner-padding">
+        {this.state.projects.map(project =>
+          <ProjectCard  key={project.id} className="w-sm"
+                        href={project.url}
+                        name={project.name}
+                        description={project.description}
+                        imageUrl="https://cdn.blizzardwatch.com/wp-content/uploads/2016/12/Gingerdread-Header-120916.jpg"
+          />
+        )}
+      </div>
     );
   }
 }
---
+++ frontend/src/components/Project/ProjectCard.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const styles = {
+    media: {
+        height: 0,
+        paddingTop: '56.25%', // 16:9
+        cursor: 'pointer'
+    }
+}
+
+class ProjectCard extends React.Component {
+    render() {
+        const { name, description, href, imageUrl, ...otherProps } = this.props;
+
+        return (
+            <div {...otherProps}>
+              <Card  className={this.props.classes.undecorated}>
+                <CardMedia
+                    onClick={() => {window.location.href = href}}
+                    className={this.props.classes.media}
+                    image={ imageUrl || '' }
+                    title="Project Image"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="headline" component="h2"
+                              className={this.props.classes.undecorated} noWrap={true}>
+                    { name }
+                  </Typography>
+                  <Typography component="p" noWrap={true}>
+                    { description }
+                  </Typography>
+                </CardContent>
+              </Card>
+            </div>
+          );
+    }
+}
+
+ProjectCard.propTypes = {
+    imageUrl: PropTypes.string,
+    name: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired
+}
+
+export default withStyles(styles)(ProjectCard);
