4f432bf9a87008befd7531eb4233ebc072aa5c29
Allan Nozomu Fukasawa <allannozomu@gmail.com>
2018-06-25 19:27:24 -0300

    Makemigrations

--- backend/stukov/stukov/tasks/models.py
+++ backend/stukov/stukov/tasks/models.py
@@ -4,11 +4,18 @@ from stukov.users.models import User
 # Create your models here.
 
 class Task(models.Model):
+    OPTIONS = (
+        ('BAC', 'Backlog'),
+        ('TOD', 'To do'),
+        ('DON', 'Doing'),
+        ('DOI', 'Done'),
+    )
+
     name = models.CharField(max_length=255, blank=False, null=False)
     date_limit = models.DateField(blank=False, null=False)
     description = models.TextField()
     priority = models.IntegerField()
-    status = models.CharField(max_length=300, blank=True, null=True)
+    status = models.CharField(max_length=3, choices=OPTIONS, default='BAC')
     finished_date = models.DateField(blank=True,null=True)
 
 class Project(models.Model):
